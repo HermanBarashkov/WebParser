40a8f586cf6c965b57143381e9bfeb416b21fdb6
german <clian555@gmail.com>
2025-01-21 19:57:26 +0300

    AlfaMax

---
+++ src/main/java/WriterExcel.java
@@ -0,0 +1,35 @@
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class WriterExcel {
+    public void writeToExcel( String result) {
+        try (FileInputStream fileIn = new FileInputStream(new File("Результат.xlsx"))){
+            Workbook workbook = WorkbookFactory.create(fileIn);
+            Sheet sheet = workbook.getSheetAt(0);
+
+            int lastRowNum = sheet.getLastRowNum();
+            Row newRow = sheet.createRow(lastRowNum + 1);
+            Cell cell =  newRow.createCell(0);
+            cell.setCellValue("Итоговая сумма");
+
+            cell = newRow.createCell(1);
+            cell.setCellValue(result);
+
+            try (FileOutputStream fileOut = new FileOutputStream("Результат.xlsx")) {
+                workbook.write(fileOut);
+                System.out.println("Данные успешно добавлены в файл!");
+            }
+
+            // Закрываем workbook
+            workbook.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
